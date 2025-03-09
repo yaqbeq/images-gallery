5507816664e5b73b25d5ed69fa98bbf6296a365a
Jakub Tuczynski <yaqb92@gmail.com>
2025-03-09 19:13:41 +0100

    Added httpx requests to unsplash API

--- api/src/main.py
+++ api/src/main.py
@@ -1,11 +1,40 @@
-from flask import Flask
+import os
 
+import httpx
+from dotenv import load_dotenv
+from flask import Flask, request
+
+# Load environment variables from .env file
+load_dotenv()
+
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPLASH_KEY = os.getenv('UNSPLASH_KEY')
+
+if not UNSPLASH_KEY:
+    raise OSError('Please create .env file with UNSPLASH_KEY')
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+def fetch_subjects(headers, params=None):
+    with httpx.Client() as client:
+        try:
+            response = client.get(UNSPLASH_URL, headers=headers, params=params)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            print(f'Error fetching subjects: {e.response.status_code}')
+            return []
+        except Exception as e:
+            print(f'Unexpected error: {e}')
+            return []
+
+
+@app.route('/new-image')
+def new_image():
+    word = request.args.get('query')
+    headers = {'Accept-Version': 'v1', 'Authorization': f'Client-ID {UNSPLASH_KEY}'}
+    params = {'query': word}
+    return fetch_subjects(headers, params)
 
 
 if __name__ == '__main__':
